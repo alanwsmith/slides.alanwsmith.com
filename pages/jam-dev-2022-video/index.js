5e27bf7812cebdea71e095078b589c53a5f16014
Alan W. Smith <alan@alanwsmith.com>
2022-01-16 18:06:39 -0500

    Adjusted spacing of the main slides

--- pages/jam-dev-2022-video/index.js
+++ pages/jam-dev-2022-video/index.js
@@ -2,10 +2,19 @@ export default function Page() {
   return (
     <div className="flex items-center justify-center h-screen pt-8 mx-auto max-w-prose text-6xl font-mono">
       <ul>
-        <li className="pb-6">monocrack.alanwsmith.com</li>
-        <li className="pb-6">ideas.alanwsmith.com</li>
-        <li className="pb-6">alanwsmith.com</li>
-        <li className="pb-28">@TheIdOfAlan</li>
+        <li className="pb-6">
+          <span className="text-blue-400">monocrack</span>.alanwsmith.com
+        </li>
+        <li className="pb-6">
+          &nbsp;&nbsp;&nbsp;&nbsp;<span className="text-blue-400">ideas</span>
+          .alanwsmith.com
+        </li>
+        <li className="pb-6">
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;alanwsmith.com
+        </li>
+        <li className="pb-28">
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;@TheIdOfAlan
+        </li>
       </ul>
     </div>
   )
