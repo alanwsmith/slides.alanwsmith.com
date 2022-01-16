02cc8205a8c276e23b606e7546c8a9fc6ebe10c4
Alan W. Smith <alan@alanwsmith.com>
2022-01-16 17:52:26 -0500

    Enlarged the font for jam.dev 2022

--- pages/jam-dev-2022-video/index.js
+++ pages/jam-dev-2022-video/index.js
@@ -1,6 +1,6 @@
 export default function Page() {
   return (
-    <div className="flex items-center justify-center h-screen pt-8 mx-auto max-w-prose text-4xl font-mono">
+    <div className="flex items-center justify-center h-screen pt-8 mx-auto max-w-prose text-6xl font-mono">
       <ul>
         <li className="pb-6">monocrack.alanwsmith.com</li>
         <li className="pb-6">ideas.alanwsmith.com</li>
